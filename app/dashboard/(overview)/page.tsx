e30424b52a6433813e78d1f1ffbb796e97e2b9a6
Emil Nilsson <emil.nilsson@dataductus.se>
2023-12-06 17:41:21 +0100

    Wrap revenuechart component in react suspense, add artificial delay for fetching revenue data

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,11 +1,12 @@
-import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '../../lib/data';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchCardData, fetchLatestInvoices } from '../../lib/data';
 import { Card } from '../../ui/dashboard/cards';
 import LatestInvoices from '../../ui/dashboard/latest-invoices';
 import RevenueChart from '../../ui/dashboard/revenue-chart';
 import { lusitana } from '../../ui/fonts';
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfCustomers,
@@ -16,7 +17,9 @@ export default async function Page() {
 
   return (
     <main>
-      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Card title="Collected" value={numberOfCustomers} type="collected" />
         <Card title="Pending" value={numberOfInvoices} type="pending" />
@@ -32,7 +35,9 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
