19bd246772da5caa30598837cc5e7208c810f4f7
Emil Nilsson <emil.nilsson@dataductus.se>
2023-12-06 17:08:36 +0100

    Add missing tw to h1 in dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -16,7 +16,7 @@ export default async function Page() {
 
   return (
     <main>
-      <h1 className={`${lusitana.className}`}>Dashboard</h1>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Card title="Collected" value={numberOfCustomers} type="collected" />
         <Card title="Pending" value={numberOfInvoices} type="pending" />
