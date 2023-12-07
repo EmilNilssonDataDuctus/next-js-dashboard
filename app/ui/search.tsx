230a7977fa52bde5c70820a9219f7baf11cf5228
Emil Nilsson <emil.nilsson@dataductus.se>
2023-12-07 16:50:14 +0100

    Force user back to page 1 after entering search query

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -12,6 +12,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const handleSearch = useDebouncedCallback((term: string) => {
     console.log(term);
     const params = new URLSearchParams(searchParams);
+    params.delete('page');
     if (term) {
       params.set('query', term);
     } else {
