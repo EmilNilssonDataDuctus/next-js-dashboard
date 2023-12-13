275fbe246d8bb1cdb4346175a6cdfe8d1d23636c
Emil Nilsson <emil.nilsson@dataductus.se>
2023-12-13 18:14:58 +0100

    Add credentials provider

--- auth.ts
+++ auth.ts
@@ -1,4 +1,8 @@
 import NextAuth from 'next-auth';
+import credentials from 'next-auth/providers/credentials';
 import { authConfig } from './auth.config';
 
-export const { auth, signIn, signOut } = NextAuth({ ...authConfig });
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [credentials({})],
+});
